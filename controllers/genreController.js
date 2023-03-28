29081b207211711b176ebb5acaa1ed8ee668cfc3
gorkemu <gorkemunal88@gmail.com>
2023-03-29 02:04:00 +0300

    handle genre update

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -169,11 +169,55 @@ exports.genre_delete_post = (req, res, next) => {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+exports.genre_update_get = (req, res, next) => {
+  Genre.findById(req.params.id, function (err, genre) {
+    if (err) {
+      return next(err);
+    }
+    if (genre == null) {
+      // No results.
+      const err = new Error("Genre not found");
+      err.status = 404;
+      return next(err);
+    }
+    // Success.
+    res.render("genre_form", { title: "Update Genre", genre: genre });
+  });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-};
+exports.genre_update_post = [
+  // Validate and sanitize the name field.
+  body("name", "Genre name required").trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request .
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data (and the old id!)
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values and error messages.
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre: genre,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // Data from form is valid. Update the record.
+    Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful - redirect to genre detail page.
+      res.redirect(thegenre.url);
+    });
+  },
+];
